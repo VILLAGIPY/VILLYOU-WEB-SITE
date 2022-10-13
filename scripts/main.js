bf61f66838e607cd7fd2b31eec37d588410d2207
Villagi <krivenkosemen281@gmail.com>
2022-10-13 20:50:44 +0300

    Beta v.0.1.4

--- scripts/main.js
+++ scripts/main.js
@@ -17,14 +17,3 @@ if(!localStorage.getItem('name')) {
 myButton1.onclick = function() {
   setUserName();
 }
-
-var myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpsiOVCytKs1TQ9n0xzZt3t_upZD1vwoVrLUbtGoGDpg&s') {
-      myImage.setAttribute ('src','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyMU0X3lL4Lb9X6Huxa5w08JpbQA7Rt94jT801d5v-Sg&s');
-    } else {
-      myImage.setAttribute ('src','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpsiOVCytKs1TQ9n0xzZt3t_upZD1vwoVrLUbtGoGDpg&s');
-    }
-}
